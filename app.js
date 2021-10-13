263146eb969371d2895958585ac95f818661050d
Marian-Janolek <m.janolek@gmail.com>
2021-10-13 18:05:34 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+// rest of the packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+// database
+const connectDB = require('./db/connect.js');
+
+// routers
+const authRouter = require('./routes/authRoutes.js');
+const userRouter = require('./routes/userRoutes');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found.js');
+const errorHandlerMiddleware = require('./middleware/error-handler.js');
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.get('/', (req, res) => {
+  res.send('e-commerce api');
+});
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port: ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
